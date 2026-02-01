b9828ae67dce84872ca6be5e7565a1502d3e201d
Darking053 <darking053@protonmail.com>
2026-02-01 19:33:58 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,42 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ThunderConfig {
+    pub homepage: String,
     pub telemetry: bool,
-    pub adblock: bool,
-    pub private_mode: bool,
 }
 
 impl Default for ThunderConfig {
     fn default() -> Self {
         Self {
+            homepage: "about:blank".into(),
             telemetry: false,
-            adblock: true,
-            private_mode: true,
         }
+    }
+}
+
+pub struct ThunderCore {
+    cfg: ThunderConfig,
+}
+
+impl ThunderCore {
+    pub fn new(cfg: ThunderConfig) -> Self {
+        Self { cfg }
+    }
+
+    pub fn init(&self) -> Result<()> {
+        log::info!("ThunderCore initialized (telemetry={})", self.cfg.telemetry);
+        Ok(())
+    }
+
+    pub fn homepage(&self) -> &str {
+        &self.cfg.homepage
+    }
+
+    pub fn navigate(&self, url: &str) -> Result<()> {
+        // TODO: Gecko FFI çağrısı
+        log::info!("Navigate -> {}", url);
+        Ok(())
     }
 }
